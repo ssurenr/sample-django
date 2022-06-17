756e862f5e3f6e26a9d40c9bd41dcafb8a1e0536
Surendhar Ravichandran <surendhar@ymail.com>
2022-06-17 22:12:56 +0100

    Authorise user

--- frontpage/views.py
+++ frontpage/views.py
@@ -1,7 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 @login_required()
 def index(request):
-    return render(request, "index.html", context={})
+    if request.user.is_staff:
+        return render(request, "index.html", context={})
+    else:
+        return HttpResponse('Permission Denied')
